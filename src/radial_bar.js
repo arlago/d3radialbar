eb6a30a220f1174311ac33c10146e8d3b843531a
Antonio Rodrigo Lago <antonio.lago@pull4up.com.br>
2015-07-22 14:41:18 -0300

    Code refactoring and creation of the examples for Today, Week and Month.

--- src/radial_bar.js
+++ src/radial_bar.js
@@ -31,7 +31,8 @@ var config = {
     outerCircle: "#e5e5e5"
   },
   outerRadius: 110,
-  labelDistance: 15 // Label distance from outerRadius.
+  labelDistance: 15, // Label distance from outerRadius.
+  labelType: "horizontal" // horizontal | circular
 };
 
 var dataModelBase = {
@@ -59,106 +60,103 @@ for (var i = 0; i < 30; i++) {
   config.data.push(currentDataModel);
 }
 
-var rotationDegree = getRotationDegree(config.data);
+function RadialBar(elementId, config) {
 
-var drawConfig = {
-  svgCenter: {
+  this.elementId = elementId;
+  this.svgCenter = {
     x: config.width /2,
     y: config.height /2
-  },
+  };
+  this.config = config;
+  this.degrees = new MathDegree(config.data.length);
 
 }
 
-var svg = d3.select("body")
-            .append("svg")
-            .attr("id", "radial0")
-            .attr("width", config.width)
-            .attr("height", config.height);
+RadialBar.prototype.draw = function() {
+  this.createSvg();
+  this.createOuterCircle();
+  this.createBars();
+  switch (this.config.labelType) {
+    case "circular":
+      this.createCircularLabels();
+    break;
+    case "horizontal":
+      this.createHorizontalLabels();
+    break;
+  }
+  this.createInnerCircle();
+  this.createCentralText();
+};
 
-// OuterCircle
-svg.append("circle")
-  .attr("cx", config.width / 2).attr("cy", config.height / 2)
-  .attr("r", config.outerRadius)
-  .attr('fill', function(d, i) {
-      return config.colors.outerCircle;
-  });
+RadialBar.prototype.createSvg = function() {
+  this.svg = d3.select("#" + this.elementId)
+              .append("svg")
+              .attr("id", "radial0")
+              .attr("width", this.config.width)
+              .attr("height", this.config.height);
+};
 
-svg.selectAll('rect').data(config.data)
-  .enter()
-  .append("rect")
-  .attr("id", function(d, i) { return "bar" + i; })
-  .attr("fill", function(d, i) { return d.bar.color; })
-  .attr("width", function(d, i) { return d.bar.value; })
-  .attr("height", function(d, i) { return d.bar.width; })
-  .attr("x", function(d, i) { return (d.bar.value + config.center.innerRadius) * -1; })
-  .attr("y", function(d, i) { return d.bar.width * -1; })
-  .attr("transform", function(d, i) {
+RadialBar.prototype.createOuterCircle = function() {
+  var self = this;
+  this.svg.append("circle")
+    .attr("cx", this.svgCenter.x)
+    .attr("cy", this.svgCenter.y)
+    .attr("r", this.config.outerRadius)
+    .attr('fill', function(d, i) {
+        return self.config.colors.outerCircle;
+    });
+};
 
-    var currentRotationDegree = (rotationDegree * i) + 90;
-    var xCorrection = (d.bar.width / 2) *  Math.sin(currentRotationDegree * Math.PI / 180);
-    var yCorrection = (d.bar.width / 2) *  Math.cos(currentRotationDegree * Math.PI / 180);
+RadialBar.prototype.createBars = function() {
+  var self = this;
+  this.svg.selectAll('rect')
+    .data(this.config.data)
+    .enter()
+    .append("rect")
+    .attr("id", function(d, i) { return "bar" + i; })
+    .attr("fill", function(d, i) { return d.bar.color; })
+    .attr("width", function(d, i) { return d.bar.value; })
+    .attr("height", function(d, i) { return d.bar.width; })
+    .attr("x", function(d, i) { return (d.bar.value + self.config.center.innerRadius) * -1; })
+    .attr("y", function(d, i) { return d.bar.width * -1; })
+    .attr("transform", function(d, i) {
 
-    var xTranslate = (config.width / 2) - xCorrection;
-    var yTranslate = (config.height / 2) + yCorrection;
+      // var currentRotationDegree = (rotationDegree * i) + 90;
+      var currentRotationDegree = self.degrees.getCurrentRotationDegree(i);
+      var xCorrection = (d.bar.width / 2) *  self.degrees.sin(currentRotationDegree);
+      // var xCorrection = (d.bar.width / 2) *  Math.sin(currentRotationDegree * Math.PI / 180);
+      var yCorrection = (d.bar.width / 2) *  self.degrees.cos(currentRotationDegree);;
+      // var yCorrection = (d.bar.width / 2) *  Math.cos(currentRotationDegree * Math.PI / 180);
 
-    return "translate(" + xTranslate + "," + yTranslate + ") rotate(" + currentRotationDegree + ")";
+      var xTranslate = self.svgCenter.x - xCorrection;
+      var yTranslate = self.svgCenter.y + yCorrection;
 
-  });
+      return "translate(" + xTranslate + "," + yTranslate + ") rotate(" + currentRotationDegree + ")";
 
-// var labelRadius = config.outerRadius + config.labelDistance;
-//var distanceFromTop = ((labelRadius * 45.45) / 100) - 10;
+    });
+};
 
-// Radius | Top distance
-// 70       90
-// 80  ===  80 -> Contant
-// 90       70
-// 100      60
-// 110      50
-// 120      40
-// 130      30
-// 140      20
-
-// var constantForTopPositioning = 80;
-// var distanceFromTop = 0;
-// if (constantForTopPositioning < labelRadius) {
-//   distanceFromTop = constantForTopPositioning - (labelRadius - constantForTopPositioning);
-// } else {
-//   distanceFromTop = constantForTopPositioning + (constantForTopPositioning - labelRadius);
-// }
-// var labelRadius = config.outerRadius + config.labelDistance;
-// var distanceFromTop = (config.height / 2) - (labelRadius);
-// var distanceFromLeft = config.width / 2;
-
-// var numberOfBars = config.data.length;
-var labels = svg.append("g")
-  .classed("labels", true);
+RadialBar.prototype.createCircularLabels = function() {
+  var self = this;
+  var labels = this.svg.append("g")
+    .classed("labels", true);
 
   labels.append("def")
     .append("path")
     .attr("id", "label-path")
-    // .attr("d", "m0 " + -labelRadius + " a" + labelRadius + " " + labelRadius + " 0 1,1 -0.01 0");
-    // .attr("d", "m160 " + 50 + " a" + 110 + " " + 110 + " 0 1,1 -0.01 0"); the correct one.
-    // m160 is the distance from the left corner.
-    // 50 is the distance from the top.
-    // 100 is the radius.
-    // ((labelRadius / 2) - config.labelDistance)
-    //var distanceFromLeftCorner = config.width / 2;
-
-    // .attr("d", "m160 " + 90 + " a" + 70 + " " + 70 + " 0 1,1 -0.01 0");
     .attr("d", function(d, i) {
-      var labelRadius = config.outerRadius + config.labelDistance;
-      var distanceFromTop = (config.height / 2) - (labelRadius);
-      var distanceFromLeft = config.width / 2;
+      var labelRadius = self.config.outerRadius + self.config.labelDistance;
+      var distanceFromTop = self.svgCenter.y - labelRadius;
+      var distanceFromLeft = self.svgCenter.x;
       return "m" + distanceFromLeft + " " + distanceFromTop + " a" + labelRadius + " " + labelRadius + " 0 1,1 -0.01 0";
-    })
-    // .attr("d", "m" + distanceFromLeft + " " + distanceFromTop + " a" + labelRadius + " " + labelRadius + " 0 1,1 -0.01 0");
+    });
 
   labels.selectAll("text")
-    .data(config.data)
+    .data(this.config.data)
     .enter()
     .append("text")
     .attr('degree', function(d, i) {
-      return (rotationDegree * i) + 90;
+      return self.degrees.getCurrentRotationDegree(i);
     })
     .attr('id', function(d, i) {
       return 'label' + i;
@@ -169,137 +167,155 @@ var labels = svg.append("g")
     .style("text-anchor", "middle")
     .append("textPath")
     .attr("xlink:href", "#label-path")
-    // .attr("startOffset", function(d, i) {return i * 100 / numberOfBars + 50 / numberOfBars + '%';}) The correct one.
     .attr("startOffset", function(d, i) {
-      return i * 100 / config.data.length + '%';
+      return i * 100 / self.config.data.length + '%';
     })
     .text(function(d, i) { return d.label.text; });
 
-/*
-svg.selectAll('text').data(config.data)
-  .enter()
-  .append("text")
-  .attr('text-anchor', 'middle')
-  .attr('degree', function(d, i) {
-    return (rotationDegree * i) + 90;
-  })
-  .attr('id', function(d, i) {
-    return 'label' + i;
-  })
-  .attr("font-family", function(d, i) { return d.label.fontFamily; } )
-  .attr("font-size", function(d, i) { return d.label.fontSize; } )
-  .attr("fill", function(d, i) { return d.label.fontColor; } )
-  .text(function (d, i) { return d.label.text; })
-  .attr("x", function(d, i) {
-    var currentRotationDegree = (rotationDegree * i) + 90;
-    var xCorrection = 0;
-    return labelHorizontal(d, i).x;
-  })
-  .attr("y", function(d, i) {
-    var currentRotationDegree = (rotationDegree * i) + 90;
-    var labelRadius = config.outerRadius + config.labelDistance;
-    var yCorrection = 0;
-    return labelHorizontal(d, i).y + this.offsetHeight / 4;
-  });
+};
 
-function labelHorizontal(d, i) {
-  var retorno = {};
-  var labelRadius = config.outerRadius + config.labelDistance;
-  var currentRotationDegree = (rotationDegree * i) + 90;
-  switch (true) {
-    case (0 <= currentRotationDegree && currentRotationDegree <= 90) || 360 < currentRotationDegree:
-        retorno.x = drawConfig.svgCenter.x - (labelRadius * Math.cos(currentRotationDegree * Math.PI / 180));
-        retorno.y = drawConfig.svgCenter.y - (labelRadius * Math.sin(currentRotationDegree * Math.PI / 180));
-      break;
+RadialBar.prototype.createHorizontalLabels = function() {
+  var self = this;
+  var labelRadius = this.config.outerRadius + this.config.labelDistance;
+  this.svg.selectAll('text')
+    .data(this.config.data)
+    .enter()
+    .append("text")
+    .attr('text-anchor', 'middle')
+    .attr('degree', function(d, i) {
+      return self.degrees.getCurrentRotationDegree(i);
+    })
+    .attr('id', function(d, i) {
+      return 'label' + i;
+    })
+    .attr("font-family", function(d, i) { return d.label.fontFamily; } )
+    .attr("font-size", function(d, i) { return d.label.fontSize; } )
+    .attr("fill", function(d, i) { return d.label.fontColor; } )
+    .text(function (d, i) { return d.label.text; })
+    .attr("x", function(d, i) {
+      return calculateLabelX(i);
+    })
+    .attr("y", function(d, i) {
+      return calculateLabelY(i) + this.offsetHeight / 4;
+    });
 
-    case 90 < currentRotationDegree && currentRotationDegree <= 180:
-        retorno.x = drawConfig.svgCenter.x + Math.abs((labelRadius * Math.cos(currentRotationDegree * Math.PI / 180)));
-        retorno.y = drawConfig.svgCenter.y - Math.abs((labelRadius * Math.sin(currentRotationDegree * Math.PI / 180)));
-      break;
+  function calculateLabelX(i) {
+    var retorno = 0;
+    var currentRotationDegree = self.degrees.getCurrentRotationDegree(i);
+    switch (true) {
+      case (0 <= currentRotationDegree && currentRotationDegree <= 90) || 360 < currentRotationDegree:
+          retorno = self.svgCenter.x - (labelRadius * self.degrees.cos(currentRotationDegree));
+        break;
 
-    case 180 < currentRotationDegree && currentRotationDegree <= 270:
-        retorno.x = drawConfig.svgCenter.x + Math.abs((labelRadius * Math.cos(currentRotationDegree * Math.PI / 180)));
-        retorno.y = drawConfig.svgCenter.y + Math.abs((labelRadius * Math.sin(currentRotationDegree * Math.PI / 180)));
-      break;
+      case 90 < currentRotationDegree && currentRotationDegree <= 180:
+          retorno = self.svgCenter.x + (labelRadius * self.degrees.cosAbs(currentRotationDegree));
+        break;
 
-    case 270 < currentRotationDegree:
-        retorno.x = drawConfig.svgCenter.x - Math.abs((labelRadius * Math.cos(currentRotationDegree * Math.PI / 180)));
-        retorno.y = drawConfig.svgCenter.y + Math.abs((labelRadius * Math.sin(currentRotationDegree * Math.PI / 180)));
-      break;
-  }
+      case 180 < currentRotationDegree && currentRotationDegree <= 270:
+          retorno = self.svgCenter.x + (labelRadius * self.degrees.cosAbs(currentRotationDegree ));
+        break;
 
-  return retorno;
-}
-*/
-
-// InnerCircle
-svg.append("circle")
-  .attr("cx", config.width / 2)
-  .attr("cy", config.height /2 )
-  .attr("r", config.center.innerRadius)
-  .attr('fill', function(d, i) {
-      return config.colors.innerCircle;
-  });
-
-svg.append("text")
-  .attr("id", "central-text")
-  .attr("text-anchor", "middle")
-  .selectAll('tspan')
-  .data(config.center.text)
-  .enter()
-  .append('tspan')
-  .text(function(d, i) {
-    return d.text;
-  })
-  .attr("dx", 0)
-  .attr("fill", function(d) { return d.color; })
-  .attr("font-size", function(d) { return d.fontSize + "px"; })
-  .attr("font-family", function(d) { return d.font; })
-  .attr('x', function(d, i) {
-    return config.width / 2;
-  })
-  .attr("dy", function(d, i) {
-    return this.offsetHeight;
-  });
-
-  svg.select("text#central-text")
-  .attr('y', function(d, i) {
-    var graphCenterY = config.height / 2;
-    var halfTextHeight = (this.offsetHeight / 2); // + config.center.textSpace;
-    return graphCenterY - halfTextHeight;
-  });
-
-function getRotationDegree(data) {
-  return 360 / data.length;
-}
-
-function getCurrentRotationDegree(d, i) {
-
-}
-
-function MathDegreesFactory() {
-  return {
-    rotationDegree: 0,
-    getRotationDegree: function(data) {
-      return 360 / data.length;
-    },
-    getCurrentRotationDegree: function(currentIndex) {
-      return rotationDegree * currentIndex;
-    },
-    cos: function(angle) {
-      return Math.cos(angle * Math.PI / 180);
-    },
-    sin: function(angle) {
-      return Math.sin(angle * Math.PI / 180);
-    },
-    cosAbs: function(angle) {
-      return Math.abs(this.cos(angle));
-    },
-    sinAbs: function(angle) {
-      return Math.abs(this.sin(angle));
+      case 270 < currentRotationDegree:
+          retorno = self.svgCenter.x - (labelRadius * self.degrees.cosAbs(currentRotationDegree));
+        break;
     }
+    return retorno;
   }
+
+  function calculateLabelY(i) {
+    var retorno = 0;
+    var currentRotationDegree = self.degrees.getCurrentRotationDegree(i);
+    switch (true) {
+      case (0 <= currentRotationDegree && currentRotationDegree <= 90) || 360 < currentRotationDegree:
+          retorno = self.svgCenter.y - (labelRadius * self.degrees.sin(currentRotationDegree));
+        break;
+
+      case 90 < currentRotationDegree && currentRotationDegree <= 180:
+          retorno = self.svgCenter.y - (labelRadius * self.degrees.sinAbs(currentRotationDegree));
+        break;
+
+      case 180 < currentRotationDegree && currentRotationDegree <= 270:
+          retorno = self.svgCenter.y + (labelRadius * self.degrees.sinAbs(currentRotationDegree));
+        break;
+
+      case 270 < currentRotationDegree:
+          retorno = self.svgCenter.y + (labelRadius * self.degrees.sinAbs(currentRotationDegree));
+        break;
+    }
+    return retorno;
+  }
+
+};
+
+RadialBar.prototype.createInnerCircle = function() {
+  var self = this;
+  this.svg.append("circle")
+    .attr("cx", self.svgCenter.x)
+    .attr("cy", self.svgCenter.y)
+    .attr("r", self.config.center.innerRadius)
+    .attr('fill', function(d, i) {
+        return self.config.colors.innerCircle;
+    });
+};
+
+RadialBar.prototype.createCentralText = function() {
+  var self = this;
+  this.svg.append("text")
+    .attr("id", "central-text")
+    .attr("text-anchor", "middle")
+    .selectAll('tspan')
+    .data(this.config.center.text)
+    .enter()
+    .append('tspan')
+    .text(function(d, i) {
+      return d.text;
+    })
+    .attr("dx", 0)
+    .attr("fill", function(d) { return d.color; })
+    .attr("font-size", function(d) { return d.fontSize + "px"; })
+    .attr("font-family", function(d) { return d.fontFamily; })
+    .attr("font-weight", function(d) { return d.fontWeight; })
+    .attr('x', function(d, i) {
+      return self.svgCenter.x;
+    })
+    .attr("dy", function(d, i) {
+      return this.offsetHeight;
+    });
+
+    this.svg.select("text#central-text")
+    .attr('y', function(d, i) {
+      var halfTextHeight = (this.offsetHeight / 2) + config.center.textSpace;
+      return self.svgCenter.y - halfTextHeight;
+    });
+};
+
+function MathDegree(dataLength) {
+  this.rotationDegree = this.getRotationDegree(dataLength);
 }
+
+MathDegree.prototype.getRotationDegree = function(dataLength) {
+  return 360 / dataLength;
+};
+
+MathDegree.prototype.getCurrentRotationDegree = function(currentIndex) {
+  return (this.rotationDegree * currentIndex) + 90;
+};
+
+MathDegree.prototype.cos = function(angle) {
+  return Math.cos(angle * Math.PI / 180);
+};
+
+MathDegree.prototype.sin = function(angle) {
+  return Math.sin(angle * Math.PI / 180);
+};
+
+MathDegree.prototype.cosAbs = function(angle) {
+  return Math.abs(this.cos(angle));
+};
+
+MathDegree.prototype.sinAbs = function(angle) {
+  return Math.abs(this.sin(angle));
+};
 
 // Math.sin(angleDegree * Math.PI / 180)
 // Math.cos(angleDegree * (180/Math.PI))
